69242efa3b6732f4cc4938ea02d0f69c9319f668
Bryan Lopez <bryloce@gmail.com>
2026-06-10 14:41:43 -0500

    Se creo la funcion de CrearMaqueta, junto a su llamado en main.go

--- Archibase/internal/handlers/maquetas.go
+++ Archibase/internal/handlers/maquetas.go
@@ -1,1 +1,33 @@
 package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"proyecto/internal/models"
+	"proyecto/internal/storage"
+)
+
+func CrearMaqueta(respuesta http.ResponseWriter, peticion *http.Request) {
+	respuesta.Header().Set("Content-Type", "application/json")
+	var nuevaMaqueta models.Maqueta
+	lector := json.NewDecoder(peticion.Body)
+	err := lector.Decode(&nuevaMaqueta)
+	if err != nil {
+		respuesta.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(respuesta).Encode(map[string]string{"error": "Datos invalidos"})
+		return
+	}
+	if nuevaMaqueta.Titulo == "" {
+		respuesta.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(respuesta).Encode(map[string]string{"error": "El titulo es obligatorio"})
+		return
+	}
+	nuevaMaqueta.ID = storage.ConteoMaquetas
+	storage.ConteoMaquetas++
+	storage.ListaMaquetas = append(storage.ListaMaquetas, nuevaMaqueta)
+	fmt.Println("--> Maqueta creada con ID:", nuevaMaqueta.ID)
+	respuesta.WriteHeader(http.StatusCreated)
+	json.NewEncoder(respuesta).Encode(nuevaMaqueta)
+}
