d60bf92b6044558f08e02a442d431f2fa3ad9577
Eduardo García <eagf5544@gmail.com>
2026-06-11 07:14:17 -0500

    cambios

--- Archibase/cmd/api/main.go
+++ Archibase/cmd/api/main.go
@@ -26,6 +26,23 @@ func main() {
 		r.Put("/{id}", handlers.ActualizarProveedor)
 		r.Delete("/{id}", handlers.EliminarProveedor)
 	})
+	// Rutas de asesor
+	enrutador.Route("/api/v1/asesores", func(r chi.Router) {
+		r.Get("/", handlers.GetAllAsesores)
+		r.Post("/", handlers.CreateAsesor)
+		r.Get("/{id}", handlers.GetAsesorByID)
+		r.Put("/{id}", handlers.UpdateAsesor)
+		r.Delete("/{id}", handlers.DeleteAsesor)
+	})
+	// Rutas de contratacion
+	enrutador.Route("/api/v1/contrataciones", func(r chi.Router) {
+		  r.Get("/", handlers.GetAllContrataciones)
+            r.Post("/", handlers.CreateContratacion)
+            r.Get("/{id}", handlers.GetContratacionByID)
+            r.Put("/{id}", handlers.UpdateContratacion)
+            r.Delete("/{id}", handlers.DeleteContratacion)
+        })
 	fmt.Println("Servidor de Archibase corriendo en http://localhost:8080")
 	http.ListenAndServe(":8080", enrutador)
-}
+	
+    }
