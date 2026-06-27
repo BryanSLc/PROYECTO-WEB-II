eada88b9eadababaa2a00733ea308f448177a6c1
Eduardo García <eagf5544@gmail.com>
2026-06-27 13:18:25 -0500

    "arreglos"

---
+++ Archibase/internal/models/contratacion.go
@@ -0,0 +1,7 @@
+type Contratacion struct {
+	IDcontratacion int    `json:"id_contratacion"`
+	Estudiante     string `json:"estudiante"`
+	Fecha          string `json:"fecha"`
+	Estado         string `json:"estado"` // "pendiente", "confirmada", "cancelada","completado"
+	IDservicio     int    `json:"id_servicio"`
+}
